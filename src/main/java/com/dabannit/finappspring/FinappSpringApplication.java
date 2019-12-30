292c2c27da48ed9a2df3af271d5db84a24b6ceb5
bloosky <blooskydiver@gmail.com>
2019-12-29 19:54:49 -0500

    initial version

---
+++ src/main/java/com/dabannit/finappspring/FinappSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dabannit.finappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinappSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinappSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dabannit/finappspring/FinappSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dabannit.finappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinappSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
